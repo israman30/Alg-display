225299d130c4ad43a9f74492c52980fe71df66ed
Israel Manzo <israelmanzo814@gmail.com>
2020-12-06 14:33:39 -0500

    customize sort buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="title">
-            <i className="far fa-chart-bar"></i> VISUALGORITHM
+            <i className="far fa-chart-bar"></i> SORTING VISUALTGO
         </div>
     )
 }
